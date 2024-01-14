49fc69e35c1a45d487efb51594d1f793aea2477a
Hellcat232 <maxkuich1993@gmail.com>
2024-01-14 02:40:50 +0200

    task-1
    is done

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,11 @@
-const category = document.querySelectorAll("#categories");
-
-category.forEach((elem) => {});
-
-console.log(category);
+const allCategories = document.getElementById("categories");
+const allItems = allCategories.children;
+console.log(`Numbers of categories: ${allItems.length}`);
+const categoryName = allCategories.querySelectorAll(".item");
+categoryName.forEach((name) => {
+  const names = name.firstElementChild;
+  console.log(`Category: ${names.textContent}`);
+  const elemNumber = names.nextElementSibling;
+  elemNumber.querySelectorAll("li");
+  console.log(`Elements: ${elemNumber.children.length}`);
+});
