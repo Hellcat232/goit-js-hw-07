5435c53fba5ddec232016fb8006b0f8902fb66c2
Hellcat232 <maxkuich1993@gmail.com>
2024-01-18 01:43:48 +0200

    квепв

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector(".change-color");
+
+btn.addEventListener("click", onBtnHandler);
+
+function onBtnHandler() {
+  const body = document.querySelector("body");
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  const outputValueColor = document.querySelector(".color");
+  outputValueColor.textContent = newColor;
+}
